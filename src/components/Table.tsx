10945be83715c9e771ae750ed8a65cb50b12b04a
Ana Laura <annalauraa2016@gmail.com>
2025-05-20 22:16:15 -0300

    Desenvolvimento evoluído

---
+++ src/components/Table.tsx
@@ -0,0 +1,65 @@
+import { ReactNode } from "react";
+import styles from "./Table.module.css";
+
+export interface ColumnType<T extends object> {
+  key: string;
+  title: string;
+  align?: "left" | "center" | "right";
+  customRender?: (value: T) => ReactNode;
+}
+
+interface TableProps<T extends object> {
+  data: T[];
+  columns: ColumnType<T>[];
+}
+
+export default function Table<T extends object>({
+  data,
+  columns,
+}: TableProps<T>) {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th
+              key={`th-${column.key}`}
+              style={{
+                borderTopLeftRadius: index === 0 ? 10 : 0,
+                borderTopRightRadius: index === columns.length - 1 ? 10 : 0,
+              }}
+            >
+              {column.title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((row, dataIndex) => (
+          <tr>
+            {columns.map((column, columnIndex) => (
+              <td
+                style={{
+                  textAlign: column.align || "center",
+                  borderBottomLeftRadius:
+                    dataIndex === data.length - 1 && columnIndex === 0 ? 10 : 0,
+                  borderBottomRightRadius:
+                    dataIndex === data.length - 1 &&
+                    columnIndex === columns.length - 1
+                      ? 10
+                      : 0,
+                }}
+                key={`td-${column.key}`}
+              >
+                {column.customRender
+                  ? column.customRender(row)
+                  : (row[column.key as keyof T] as ReactNode)}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
